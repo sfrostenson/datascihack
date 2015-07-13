8a22985a99a9ea2fb25f63340fe859e944330e0e
Sarah Frostenson <sarah.e.frostenson@gmail.com>
2015-07-13 13:30:00 -0400

    updated some v. shitty last minute code for better functionality

--- js/persona.js
+++ js/persona.js
@@ -1,28 +1,37 @@
 $(document).ready(function() {
-	$('#jack').click(function() {
-		$('.hardcode-highlight-fr').html('He is calling for a ban on the free drinks served by Magaluf’s hotels to attract visitors there on 10-day, $500 all-inclusive packages.')
-	})
-	$('#daisy').click(function() {
-		$('.hardcode-highlight-fr').html('Some of the incidents go beyond carelessness and show a lack of respect for local mores.')
-	})
-	$('#locke').click(function() {
-		$('.hardcode-highlight-fr').html('Some of the acts can also be self-destructive, as seen in the proliferation in Magaluf and other resort towns of “balconing” — when inebriated tourists jump between balconies or dive into courtyard pools, often resulting in injury or even death.')
-	})
+	// top friend highlights
+	var quotes = {
+		daisy: 'He is calling for a ban on the free drinks served by Magaluf’s hotels to attract visitors there on 10-day, $500 all-inclusive packages.',
+		jack: 'Some of the incidents go beyond carelessness and show a lack of respect for local mores.',
+		locke:'Some of the acts can also be self-destructive, as seen in the proliferation in Magaluf and other resort towns of “balconing” — when inebriated tourists jump between balconies or dive into courtyard pools, often resulting in injury or even death.'
+	};
 
-	$('.everyone').click(function() {
-		$('.text').css("display", "none");
-		$('.text2').css("display", "inline-block");
-		$('.text2').append("<img src='img/jack.png' style='width: 20%;' />");
-		$('.text2').append('They could do better than this');
-	})
-	$('.friends').click(function() {
-		$('.text2').css("display", "none");
-		$('.text').css("display", "inline-block");
-		$('.text').append("<img src='img/locke.png' style='width: 20%;' />");
-		$('.text').append('Kids these days');
-		$('.text').append('<br>');
-		$('.text').append("<img src='img/daisy.png' style='width: 20%;' />");
-		$('.text').append('So narcissistic');
-		
-	})
+	var comments = {
+		'everyone': {
+			person: [
+				["<p><img src='img/jack.png' style='width: 20%; padding-right:5px; padding-top:5px;' /> They could do better than this</p>"]
+			]
+		},
+		'friends': {
+			person: [
+				["<p><img src='img/locke.png' style='width: 20%; padding-right:5px; padding-top:5px;' /> Kids these days</p>" +
+				"<p><img src='img/daisy.png' style='width: 20%; padding-right:5px; padding-top:5px;' /> So narcissistic</p>"]
+			]
+		}
+	};
+	// on click of img, return top friend highlight
+	$('img.persona').click(function() {
+		var $target = $(this).attr('id');
+		$('.hardcode-highlight-fr').html(quotes[$target]);
+	});
+
+	// on click of friends/everyone toggle display comments & icons
+	$('button').click(function() {
+		$('.text').css('display', 'block');
+		var $target= $(this).attr('id');
+		var array = comments[$target].person;
+		for(var i = 0; i < array.length; i++) {
+			$('.text').html(array[i]);
+		}
+	});
 });
